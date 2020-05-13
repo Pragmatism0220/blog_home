23abacdd593f9ddc12d8a29b9c2f1d2540d0859d
Pragmatism0220 <pragmatism0220@gmail.com>
2020-05-13 14:53:12 +0800

    add platelet model

--- live2d/js/waifu.js
+++ live2d/js/waifu.js
@@ -33,7 +33,7 @@ $(document).on('copy', function (){
 function initTips() {
     $.ajax({
         cache: true,
-        url: "live2d/message.json",
+        url: "https://cdn.jsdelivr.net/gh/Pragmatism0220/blog_home/live2d/message.json",
         dataType: "json",
         success: function (result) {
             $.each(result.mouseover, function (index, tips) {
